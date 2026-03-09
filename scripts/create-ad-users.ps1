1f5a3958e444bab1a650a3a6c831fab35dfbca66
Harshitha16 <hpoppala@gmail.com>
2026-03-09 18:36:32 +1100

    Update create-ad-users.ps1

--- scripts/create-ad-users.ps1
+++ scripts/create-ad-users.ps1
@@ -1,1 +1,15 @@
 
+# Import users from CSV file 
+$users = Import-Csv "users.csv" foreach ($user in $users)
+{
+New-ADUser ` 
+-Name $user.Name `
+-GivenName $user.FirstName ` 
+-Surname $user.LastName ` 
+-Department $user.Department `
+-SamAccountName $user.Username ` 
+-UserPrincipalName "$($user.Username)@corp.local" ` 
+-AccountPassword (ConvertTo-SecureString "P@ss1234" -AsPlainText -Force) `
+-Enabled $true 
+Write-Host "User created:" $user.Username 
+}
